f93d87cd1511e83d06cc35c5df18fd9a289dd333
vsparrow <khan@vsparrow.com>
2017-04-30 05:09:50 +0000

    Setup initial structure, and flow

---
+++ wikipedia.js
@@ -0,0 +1,9 @@
+// user clicks search
+// user types in earch term
+// user presses enter (or button) to contiue
+// app reaches out to wikipedia api
+// api responds
+// app pareses api
+// app displays api results
+// user clicks a result and gets redirected to link in another tabs
+
